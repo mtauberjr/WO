85fea7e2d0909f639e17f6deadc040218b0fda04
mtauberjr <github.unadorned083@passmail.net>
2025-05-29 11:21:47 -0500

    Update CopyUser.ps1

--- CopyUser.ps1
+++ CopyUser.ps1
@@ -28,10 +28,38 @@ start-Sleep 2
 $wshell.SendKeys("{ENTER}")
 start-Sleep 2
 
-cmd /c "rundll32.exe keymgr.dll,KRShowKeyMgr"
+#cmd /c "rundll32.exe keymgr.dll,KRShowKeyMgr"
 
 $Shell = New-Object -ComObject "WScript.Shell"
 $Button = $Shell.Popup("After exporting Chrome passwords and bookmarks, click OK to copy the files.", 0, "Export", 0)
+
+# Chrome Automation 
+$proc = 'Edge'
+Start-Process $proc
+Start-Sleep 2
+$wshell = New-Object -ComObject wscript.shell;
+$wshell.AppActivate("$proc")
+Start-Sleep 2
+$wshell.SendKeys("^{t}")
+start-Sleep 3
+$wshell.SendKeys('edge://wallet/passwords?source=assetsSettingsPasswords')
+start-Sleep 3
+$wshell.SendKeys("{ENTER}")
+start-Sleep 2
+$wshell.SendKeys("^{t}")
+start-Sleep 3
+$wshell.SendKeys('edge://favorites/')
+start-Sleep 2
+$wshell.SendKeys("{ENTER}")
+start-Sleep 2
+
+$Shell = New-Object -ComObject "WScript.Shell"
+$Button = $Shell.Popup("After exporting Edge passwords and bookmarks, click OK to copy the files.", 0, "Export", 0)
+
+cmd /c "rundll32.exe keymgr.dll,KRShowKeyMgr"
+
+$Shell = New-Object -ComObject "WScript.Shell"
+$Button = $Shell.Popup("After exporting Windows passwords, click OK to copy the files.", 0, "Export", 0)
 
 # List of special folders to back up
 $specialFolders = @(
